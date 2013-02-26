bb4b56030d74a844ad4a291b71c22530bb930267
phooky <phooky@gmail.com>
2013-02-26 20:26:01 +0000

    left out quad_test.cc

---
+++ bone/servo/quad_test.cc
@@ -0,0 +1,19 @@
+#include "quadrature.h"
+#include <iostream>
+using namespace std;
+int main() {
+  Quadrature q;
+
+  q.start();
+
+  for (int i = 0; i < 10000; i++) {
+	  Report r = q.getNextReport();
+	  cout << "Report: ";
+	  for (int j = 0; j < 6; j++) {
+		  cout << j << ":" << r.state[j].position << " ";
+	  }
+	  cout << endl;
+  }
+  q.stop();
+}
+
