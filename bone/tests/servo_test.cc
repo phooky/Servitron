1dd5c918f930ffb4a255416bfdc763eae6a051ea
Beaglebone <phooky@gmail.com>
2013-04-28 01:04:43 +0000

    all axis working

--- bone/tests/servo_test.cc
+++ bone/tests/servo_test.cc
@@ -2,7 +2,7 @@
 #include "brake.h"
 #include <iostream>
 
-#define SERVO 5
+#define SERVO 1
 int main() {
   // MoveBlock block(5.0,2.0,1.0,0.4,0.4);
   // float delta = 0.1;
@@ -19,20 +19,21 @@ int main() {
   Quadrature q;
   Brake b;
   b.init();
-  b.setBrake(true);
+  b.setBrake(false);
   pwm.init();
   Motor m(pwm);
   m.init();
   q.init();
   Servo servo(SERVO,q,m);
   servo.pid().setK(4,0.01,0.001);
-  servo.setPoint(5000);
+  servo.setPoint(2000);
   for (int i = 0; i < 5000; i++) {
 	  Report r = q.getNextReport();
           servo.update(r.state[SERVO].position);
 	  std::cout << r.state[SERVO].position << std::endl;
   }
   m.setPower(SERVO,0);
+  b.setBrake(true);
   q.shutdown();
 
 }
