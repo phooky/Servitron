5e236b9d2fa008398669652a3034f2b04b84acf9
Adam Mayer <me@phooky.name>
2013-05-13 15:34:53 -0400

    adding simple socket server

---
+++ servotron/server/server.c
@@ -0,0 +1,46 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <errno.h>
+#include <unistd.h>
+
+#include <string.h>
+#include <iostream>
+
+using namespace std;
+
+#ifndef SOCKET_NAME
+#define SOCKET_NAME "/var/run/servotron"
+#endif
+
+const char* socket_name = SOCKET_NAME;
+
+int startSocketServer(const char* sockName) {
+  unsigned int listenSock = socket(AF_UNIX, SOCK_STREAM, 0);
+  struct sockaddr_un listenSockaddr;
+  listenSockaddr.sun_family = AF_UNIX;
+  // Set up abstract socket namespace; Linux-only but saves us from
+  // mucking around with unlink() and multiple servers running
+  // simultaneously.
+  strcpy(listenSockaddr.sun_path+1,sockName);
+  listenSockaddr.sun_path[0]='\0';
+  size_t len = sizeof(listenSockaddr.sun_family) + 1 + strlen(sockName);
+  if (bind(listenSock, (struct sockaddr*)&listenSockaddr, len) == -1) {
+    cerr << "Could not bind " << sockName << ": " <<strerror(errno) <<endl;
+    return -1;
+  }
+  listen(listenSock,2);
+  while(1) {
+    // Accept one client at a time.
+    struct sockaddr_un inSockaddr;
+    socklen_t len = sizeof(inSockaddr);
+    int inSock = accept(listenSock, (struct sockaddr*)&inSockaddr, &len);
+    write(inSock,"GOODBYE\n",8);
+    close(inSock);
+  }
+  return 0;
+}
+
+int main(int argc, char*argv[]) {
+  return startSocketServer(socket_name);
+}
