7737eb4ae72061ec88a9cb2a63df8446aa76fcbe
Beaglebone <phooky@gmail.com>
2013-05-17 16:50:03 +0000

    starting in on pinmuxing

--- servitron/server/pinmux.cc
+++ servitron/server/pinmux.cc
@@ -1,6 +1,26 @@
 #include "pinmux.h"
 
+#include <string>
+#include <fstream>
+
+bool writePath(std::string path, int value, bool hex = true) {
+    std::ofstream outf;
+    outf.open(path.c_str());
+    if (hex) { outf << std::hex; }
+    outf << value;
+    outf.close();
+    return true;
+}
+
 bool mux::mux_all_pins() {
-	return true;
+    // mux all pwm pins
+    writePath("/sys/kernel/debug/omap_mux/mcasp0_fsx", 1); // 0:0
+    writePath("/sys/kernel/debug/omap_mux/mcasp0_aclkx", 1); // 0:1
+    writePath("/sys/kernel/debug/omap_mux/gpmc_a2", 6); // 1:0
+    writePath("/sys/kernel/debug/omap_mux/gpmc_a3", 6); // 1:1
+    writePath("/sys/kernel/debug/omap_mux/gpmc_ad8", 4); // 2:0
+    writePath("/sys/kernel/debug/omap_mux/gpmc_ad9", 4); // 2:1
+
+    return true;
 }
 
