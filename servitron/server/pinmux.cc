f0edc7e0b779ebe1f9db41c54a097182719a1f96
Adam Mayer <me@phooky.name>
2013-05-17 13:01:27 -0400

    all pins muxed

--- servitron/server/pinmux.cc
+++ servitron/server/pinmux.cc
@@ -20,7 +20,27 @@ bool mux::mux_all_pins() {
     writePath("/sys/kernel/debug/omap_mux/gpmc_a3", 6); // 1:1
     writePath("/sys/kernel/debug/omap_mux/gpmc_ad8", 4); // 2:0
     writePath("/sys/kernel/debug/omap_mux/gpmc_ad9", 4); // 2:1
-
+    // mux brake
+    writePath("/sys/kernel/debug/omap_mux/gpmc_ad10", 0x0f);
+    // mux quadrature pins
+    writePath("/sys/kernel/debug/omap_mux/gpmc_ad6", 0x3f);
+    writePath("/sys/kernel/debug/omap_mux/gpmc_ad7", 0x3f);
+    writePath("/sys/kernel/debug/omap_mux/gpmc_ad2", 0x3f);
+    writePath("/sys/kernel/debug/omap_mux/gpmc_ad3", 0x3f);
+    writePath("/sys/kernel/debug/omap_mux/gpmc_ad13", 0x3f);
+    writePath("/sys/kernel/debug/omap_mux/gpmc_ad12", 0x3f);
+    writePath("/sys/kernel/debug/omap_mux/gpmc_ad15", 0x3f);
+    writePath("/sys/kernel/debug/omap_mux/gpmc_ad14", 0x3f);
+    writePath("/sys/kernel/debug/omap_mux/gpmc_csn2", 0x3f);
+    writePath("/sys/kernel/debug/omap_mux/gpmc_csn1", 0x3f);
+    writePath("/sys/kernel/debug/omap_mux/gpmc_ad5", 0x3f);
+    writePath("/sys/kernel/debug/omap_mux/gpmc_ad4", 0x3f);
+    writePath("/sys/kernel/debug/omap_mux/gpmc_ad1", 0x3f);
+    writePath("/sys/kernel/debug/omap_mux/gpmc_ad0", 0x3f);
+    writePath("/sys/kernel/debug/omap_mux/gpmc_cns0", 0x3f);
+    writePath("/sys/kernel/debug/omap_mux/gpmc_ben1", 0x3f);
+    writePath("/sys/kernel/debug/omap_mux/gpmc_a0", 0x3f);
+    writePath("/sys/kernel/debug/omap_mux/gpmc_a1", 0x3f);
     return true;
 }
 
